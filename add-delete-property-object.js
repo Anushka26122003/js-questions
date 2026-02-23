139d69a35600d116c8d7df353976dd682bcd7a43
anushkahardkoretech-byte <anushka.hardkore.tech@gmail.com>
2026-02-23 16:35:36 +0530

    object

--- add-delete-property-object.js
+++ add-delete-property-object.js
@@ -7,3 +7,10 @@ console.log(obj);
 obj.gender = "male";
 console.log(obj);
 delete obj.gender;
+
+console.log([1, 2, 3].reduce((a, b) => a + b));//adds numbers in the array, together using the reduce 
+console.log('gfg'.repeat(3));
+//creates a new string by repeating the original string in 3 times
+console.log('6' - 1);
+//'6' is a string, so when you use the '-' operator with string and number, JavaScript convert the string to a number automatically which is called type coercion. '6' gets converted to the number 6, then 6 - 1 = 5. So, 5 is the answer.
+
